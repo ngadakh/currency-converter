2bb39e9cf21c6b21c712ff6b52c18fbc4b12122c
Navnath Gadakh <navnathgadakh@gmail.com>
2022-07-16 20:43:24 +0530

    Add wallet feature 	1. Add /wallet route to handle GET and POST methods 	2. Add wallet HTML file to show on UI

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 
 from sqlalchemy.exc import IntegrityError
 from wtforms import Form, StringField, PasswordField, validators, EmailField, SelectField
@@ -31,9 +32,8 @@ class EditUserRegistrationForm(Form):
     """Edit user registration form to load as HTML with fields"""
     username = StringField('Username', [validators.DataRequired()])
     email = EmailField('Email Address', [validators.DataRequired(), validators.Email()])
-    profile_photo = FileField('Profile Photo', [FileAllowed(['jpg', 'png'], 'Select images only!')])
+    profile_photo = FileField('Profile Photo', [FileRequired(), FileAllowed(['jpg', 'png'], 'Select images only!')])
     default_currency = SelectField('default_currency', choices=CurrencyConverter(app).get_all_currencies())
-
 
 class UserLoginForm(Form):
     """User login form"""
@@ -42,7 +42,8 @@ class UserLoginForm(Form):
 
 # User model
 class User(db.Model):
-    """User database model"""
+    """User database model""" 
+    __tablename__ = 'user'
     id  = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100), unique=True, nullable = False)
     password = db.Column(db.String(100), nullable = False)
@@ -56,6 +57,24 @@ class User(db.Model):
         self.email = email
         self.profile_photo_url = photo_url
         self.default_currency = default_currency
+
+
+class UserWalletForm(Form):
+    """User wallet where user can transfer and add money"""
+    amount = StringField("Amount", [validators.DataRequired()])
+
+
+# User wallet model
+class UserWallet(db.Model):
+    """User wallet database model"""
+    __tablename__ = 'user_wallet'
+    wallet_id = db.Column(db.Integer, primary_key = True)
+    user_id =  db.Column(db.Integer, db.ForeignKey('user.id'))
+    amount = db.Column(db.Integer, nullable = False)
+
+    def __init__(self, user_id, amount):
+        self.user_id = user_id
+        self.amount = amount
 
 
 @app.route("/")
@@ -153,3 +172,40 @@ def profile():
         else:
             app.logger.error("User not found in the database, you might have updated the user's info.")
             return redirect(url_for('logout'))
+
+
+@app.route('/wallet', methods =['GET', 'POST'])
+@login_required
+def wallet():
+    """User wallet where user can transfer money to another user"""
+    form = UserWalletForm(request.form)
+    user = db.session.query(User).filter_by(username=session['username']).first()
+    user_wallet = db.session.query(UserWallet).filter_by(user_id=user.id).first()
+    if request.method == 'POST':
+        try:
+            user_wallet = db.session.query(UserWallet).filter_by(user_id=user.id).first()
+            if user_wallet:
+                # TODO: Try to use db.session.merge()
+                new_amount = int(user_wallet.amount) + int(form.amount.data)
+                user_wallet.amount = new_amount
+                db.session.commit()
+                form.amount.data = new_amount
+            else:
+                amount = UserWallet(user.id, form.amount.data)
+                db.session.add(amount)
+                db.session.commit()
+            msg = "Amount added to wallet successfully"
+            app.logger.info(msg)
+            flash(msg)
+            return redirect(url_for('profile'))
+        except Exception as e:
+            db.session.rollback()
+            err_msg = "Failed to add money to wallet"
+            app.logger.error(err_msg, e)
+            flash(err_msg)
+    else:
+        form.amount.data = 0
+        if user_wallet:
+            form.amount.data = user_wallet.amount
+    return render_template("user_wallet.html", user=user, form=form)
+
